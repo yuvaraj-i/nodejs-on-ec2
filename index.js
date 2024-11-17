57ba89e32782d667957575e9bdbc25522b0f8eea
Rajani Ekunde <43741838+rajani103@users.noreply.github.com>
2024-11-17 15:54:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
-  res.write('** Welcome to GlobalLogic!!!! **'); //write a response to the client
+  res.write('** Welcome to HeroVired!!!! **'); //write a response to the client
   res.end(); //end the response
 }).listen(80); //the server object listens on port 80

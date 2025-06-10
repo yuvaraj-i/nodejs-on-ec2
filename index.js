d5e240ba7dac78b3f9110e618163573d1ab39f57
Yuvaraj I <yuvaraji@presidio.com>
2025-06-10 21:41:17 +0530

    ADD logs and 2 test endpoints

--- index.js
+++ index.js
@@ -2,6 +2,19 @@ var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
+  if (req.method === 'GET' && req.url === '/hello') {
+    console.log("Hello World from /hello")
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello from /hello');
+    return;
+  }
+  if (req.method === 'GET' && req.url === '/test') {
+    console.log("Hello World from /test")
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('This is the /test endpoint');
+    return;
+  }
+  console.log("Hello World")
   res.write('** Welcome to GlobalLogic!!!! **'); //write a response to the client
   res.end(); //end the response
 }).listen(80); //the server object listens on port 80
